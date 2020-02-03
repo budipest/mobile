26ca9121f85e514a87e79e4a8c5bf166a2efeeeb
danielgrgly <danielgrgly@gmail.com>
2020-02-03 21:17:31 +0100

    show "closed" for closed-state rows (addtoilet)

--- lib/ui/widgets/openHourRow.dart
+++ lib/ui/widgets/openHourRow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 
 import '../../core/common/openHourUtils.dart';
 import '../../core/common/variables.dart';
@@ -10,8 +11,7 @@ class OpenHourRow extends StatefulWidget {
   final int index;
 
   @override
-  _OpenHourRowState createState() =>
-      _OpenHourRowState();
+  _OpenHourRowState createState() => _OpenHourRowState();
 }
 
 class _OpenHourRowState extends State<OpenHourRow> {
@@ -61,82 +61,102 @@ class _OpenHourRowState extends State<OpenHourRow> {
             ),
           ),
           Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 15.0),
-                child: DropdownButton<int>(
-                  value: widget.openHours[widget.index],
-                  elevation: 16,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'Muli',
+            child: isOn
+                ? Container(
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(5),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 15.0),
+                              child: DropdownButton<int>(
+                                value: widget.openHours[widget.index],
+                                elevation: 16,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Muli',
+                                ),
+                                icon: Icon(null),
+                                isExpanded: true,
+                                underline: Container(height: 0),
+                                onChanged: (int newValue) {
+                                  widget.openHours[widget.index] = newValue;
+                                  widget.onChange(widget.openHours);
+                                },
+                                items: possibleOpenHours
+                                    .map<DropdownMenuItem<int>>((int value) {
+                                  String valueAsString =
+                                      minuteToHourFormat(value);
+                                  return DropdownMenuItem<int>(
+                                    value: value,
+                                    child: Text(valueAsString),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 17.5),
+                          child: Text(
+                            "-",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(5),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 15.0),
+                              child: DropdownButton<int>(
+                                value: widget.openHours[widget.index + 1],
+                                elevation: 16,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Muli',
+                                ),
+                                icon: Icon(null),
+                                underline: Container(height: 0),
+                                onChanged: (int newValue) {
+                                  widget.openHours[widget.index + 1] = newValue;
+                                  widget.onChange(widget.openHours);
+                                },
+                                items: possibleOpenHours
+                                    .map<DropdownMenuItem<int>>((int value) {
+                                  String valueAsString =
+                                      minuteToHourFormat(value);
+                                  return DropdownMenuItem<int>(
+                                    value: value,
+                                    child: Text(valueAsString),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Center(
+                    child: Text(
+                      FlutterI18n.translate(context, "closed"),
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
                   ),
-                  icon: Icon(null),
-                  isExpanded: true,
-                  underline: Container(height: 0),
-                  onChanged: (int newValue) {
-                    widget.openHours[widget.index] = newValue;
-                    widget.onChange(widget.openHours);
-                  },
-                  items:
-                      possibleOpenHours.map<DropdownMenuItem<int>>((int value) {
-                    String valueAsString = minuteToHourFormat(value);
-                    return DropdownMenuItem<int>(
-                      value: value,
-                      child: Text(valueAsString),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 17.5),
-            child: Text(
-              "-",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 15.0),
-                child: DropdownButton<int>(
-                  value: widget.openHours[widget.index + 1],
-                  elevation: 16,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'Muli',
-                  ),
-                  icon: Icon(null),
-                  underline: Container(height: 0),
-                  onChanged: (int newValue) {
-                    widget.openHours[widget.index + 1] = newValue;
-                    widget.onChange(widget.openHours);
-                  },
-                  items:
-                      possibleOpenHours.map<DropdownMenuItem<int>>((int value) {
-                    String valueAsString = minuteToHourFormat(value);
-                    return DropdownMenuItem<int>(
-                      value: value,
-                      child: Text(valueAsString),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
           ),
         ],
       ),
