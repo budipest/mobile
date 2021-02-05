807d207669c9597989e94eeb43b760c35a683573
dnlgrgly <dnlgrgly@gmail.com>
2021-02-05 11:08:06 +0100

    new machine commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/gergelydaniel/Documents/bin/flutter"
+export "FLUTTER_ROOT=/Users/gergelydaniel/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/gergelydaniel/Documents/GitHub/budipest-mobile"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/gergelydaniel/Documents/GitHub/budipest-mobile/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=2.0.1"
-export "FLUTTER_BUILD_NUMBER=13"
+export "FLUTTER_BUILD_NUMBER=14"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
-export "TREE_SHAKE_ICONS=true"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=/Users/gergelydaniel/Documents/GitHub/budipest-mobile/.dart_tool/package_config.json"
